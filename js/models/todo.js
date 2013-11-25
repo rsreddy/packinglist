3bf67113b27b2a89499242e5c02200584314bc2e
rsreddy <radhika.b.shah@gmail.com>
2013-11-25 16:40:27 -0600

    first commit

---
+++ js/models/todo.js
@@ -0,0 +1,441 @@
+Todos.Todo = DS.Model.extend({
+	name: DS.attr('string'),
+	category: DS.attr('string'),
+	title: DS.attr('string'),
+	quantity: DS.attr('string'),
+	isCompleted: DS.attr('boolean')
+});
+
+Todos.Todo.FIXTURES=[
+{
+	id: 1,
+	name: 'Roshan',
+	category: 'ID',
+	title: 'Birth Certificate',
+	quantity: '0',
+	isCompleted: false
+},
+{
+	id: 2,
+	name: 'Roshan',
+	category: 'Underwear',
+	title: 'Regular Diapers',
+	quantity: '60',
+	isCompleted: false
+},
+{
+	id: 3,
+	name: 'Roshan',
+	category: 'Underwear',
+	title: 'Swim Diapers',
+	quantity: 'Full pack',
+	isCompleted: false
+},
+{
+	id: 4,
+	name: 'Roshan',
+	category: 'Underwear',
+	title: 'Night Diapers',
+	quantity: '10',
+	isCompleted: false
+},
+{
+	id: 5,
+	name: 'Roshan',
+	category: 'Underwear',
+	title: 'Wipes',
+	quantity: '2 Packs'
+	isCompleted: false
+},
+{
+	id: 6,
+	name: 'Roshan',
+	category: 'Underwear',
+	title: 'Vaseline',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 7,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Short Sleeves/Onesies',
+	quantity: '17/5'
+	isCompleted: false
+},
+{
+	id: 8,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Long Sleeves',
+	quantity: '4'
+	isCompleted: false
+},
+{
+	id: 9,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Shorts',
+	quantity: '10',
+	isCompleted: false
+},
+{
+	id: 10,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Long Pants',
+	quantity: '5',
+	isCompleted: false
+},
+{
+	id: 11,
+	name: 'Roshan',
+	category: 'Swim',
+	title: 'Swim Trunks',
+	quantity: ' ',
+	isCompleted: false
+},
+{
+	id: 12,
+	name: 'Roshan',
+	category: 'Swim',
+	title: 'Floaties',
+	quantity: ' ',
+	isCompleted: false
+},
+{
+	id: 13,
+	name: 'Roshan',
+	category: 'Bibs',
+	title: 'Plastic',
+	quantity: '4',
+	isCompleted: false
+},
+{
+	id: 14,
+	name: 'Roshan',
+	category: 'Bibs',
+	title: 'Burp Cloths',
+	quantity: '4',
+	isCompleted: false
+},
+{
+	id: 15,
+	name: 'Roshan',
+	category: 'Bibs',
+	title: 'Thick',
+	quantity: '5',
+	isCompleted: false
+},
+{
+	id: 16,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Socks',
+	quantity: '10',
+	isCompleted: false
+},
+{
+	id: 17,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Nike Jacket',
+	quantity: ' ',
+	isCompleted: false
+},
+{
+	id: 18,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Hooded Sweatshirt',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 19,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Hat',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 20,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Scandals',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 21,
+	name: 'Roshan',
+	category: 'Clothing',
+	title: 'Shoes',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 22,
+	name: 'Roshan',
+	category: 'Underwear',
+	title: 'Pull-Ups',
+	quantity: '1 Packet',
+	isCompleted: false
+},
+{
+	id: 23,
+	name: 'Roshan',
+	category: 'Underwear',
+	title: 'Changing Pad',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 24,
+	name: 'Roshan',
+	category: 'Bath',
+	title: 'Towels',
+	quantity: '3',
+	isCompleted: false
+},
+{
+	id: 25,
+	name: 'Roshan',
+	category: 'Other',
+	title: 'Thin Blanket',
+	quantity: '3',
+	isCompleted: false
+},
+{
+	id: 26,
+	name: 'Roshan',
+	category: 'Other',
+	title: 'Thick Blanket',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 27,
+	name: 'Roshan',
+	category: 'Food (Airplane)',
+	title: 'Milk',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 28,
+	name: 'Roshan',
+	category: 'Food',
+	title: 'Goldfish',
+	quantity: '5 packs',
+	isCompleted: false
+},
+{
+	id: 29,
+	name: 'Roshan',
+	category: 'Food Supplies',
+	title: 'Spoons',
+	quantity: '4',
+	isCompleted: false
+},
+{
+	id: 30,
+	name: 'Roshan',
+	category: 'Food',
+	title: 'Parle G',
+	quantity: '3 packs',
+	isCompleted: false
+},
+{
+	id: 31,
+	name: 'Roshan',
+	category: 'Food Supplies',
+	title: 'Bottles',
+	quantity: '3',
+	isCompleted: false
+},
+{
+	id: 32,
+	name: 'Roshan',
+	category: 'Food Supplies',
+	title: 'Bottle Brush',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 33,
+	name: 'Roshan',
+	category: 'Food Supplies',
+	title: 'Sippy Cups',
+	quantity: '3',
+	isCompleted: false
+},
+{
+	id: 34,
+	name: 'Roshan',
+	category: 'Food Supplies',
+	title: 'Dishwashing Soap',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 35,
+	name: 'Roshan',
+	category: 'Food',
+	title: 'Pouch Food',
+	quantity: '10',
+	isCompleted: false
+},
+{
+	id: 36,
+	name: 'Roshan',
+	category: 'Food Supplies',
+	title: 'Thermos',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 37,
+	name: 'Roshan',
+	category: 'Food',
+	title: 'Water',
+	quantity: '10',
+	isCompleted: false
+},
+{
+	id: 38,
+	name: 'Roshan',
+	category: 'Entertainment',
+	title: 'Galaxy 10',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 39,
+	name: 'Roshan',
+	category: 'Entertainment',
+	title: 'Books',
+	quantity: '3',
+	isCompleted: false
+},
+{
+	id: 40,
+	name: 'Roshan',
+	category: 'Entertainment',
+	title: 'Traveling Alphabet/Numbers',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 41,
+	name: 'Roshan',
+	category: 'Entertainment',
+	title: 'Download Movies/Shows',
+	quantity: '5',
+	isCompleted: false
+},
+{
+	id: 42,
+	name: 'Reddy General',
+	category: 'Linens',
+	title: 'Pillow Cases',
+	quantity: '3',
+	isCompleted: false
+},
+{
+	id: 43,
+	name: 'Reddy General',
+	category: 'Linens',
+	title: 'Sheets',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 44,
+	name: 'Reddy General',
+	category: 'Linens',
+	title: 'Blanket',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 45,
+	name: 'Reddy General',
+	category: 'Tolietries',
+	title: 'Toothpaste',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 46,
+	name: 'Reddy General',
+	category: 'Tolietries',
+	title: 'Toothbrush',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 47,
+	name: 'Reddy General',
+	category: 'Tolietries',
+	title: 'Lotion',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 48,
+	name: 'Reddy General',
+	category: 'Tolietries',
+	title: 'Face Wash',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 49,
+	name: 'Reddy General',
+	category: 'Tolietries',
+	title: 'Face Lotion',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 50,
+	name: 'Reddy General',
+	category: 'Tolietries',
+	title: 'Soap',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 51,
+	name: 'Reddy General',
+	category: 'Outdoor',
+	title: 'Sunscreen Face Lotion',
+	quantity: '2',
+	isCompleted: false
+},
+{
+	id: 52,
+	name: 'Reddy General',
+	category: 'Outdoor',
+	title: 'Sunscreen Lotion',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 53,
+	name: 'Reddy General',
+	category: 'Outdoor',
+	title: 'Sunscreen Body Mist',
+	quantity: '1',
+	isCompleted: false
+},
+{
+	id: 54,
+	name: 'Reddy General',
+	category: 'Bath',
+	title: 'Wash Cloths',
+	quantity: '5',
+	isCompleted: false
+}]
